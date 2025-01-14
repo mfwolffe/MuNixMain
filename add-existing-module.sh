083473684502b14a1e04e2049dc25a0ceb422143
mfwolffe <wolffemf@dukes.jmu.edu>
2025-01-14 16:02:52 -0500

    script to add existing repo as submod here (several will need this)

---
+++ add-existing-module.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -x
+
+if [ ! $# -eq 2 ]; then 
+  printf "\nmissing new module name\n" >&2
+  exit 1
+fi
+
+module_ref="${1}"
+shift
+
+case "${1}" in
+  "ssh" | "SSH")
+    module_url="git@github.com:mfwolffe/${module_ref}.git" ;;
+  "https" | "HTTPS")
+    module_url="https://github.com/mfwolffe/${module_ref}.git"
+esac
+
+git submodule add "${module_url}"
+git submodule update --init --recursive
+
+git add "${module_ref}"
+
+
