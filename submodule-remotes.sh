58922ce0f1fb65e00b2c151d347ebab2dcac8c73
mfwolffe <wolffemf@dukes.jmu.edu>
2025-01-14 12:42:22 -0500

    script to automate submod remote setup

---
+++ submodule-remotes.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+remote_adder () {
+	git remote add origin "${1}"
+	git push -u origin trunk
+}
+
+for d in */ ; do
+	# chop off trailing slash and store remote name
+	remote=`echo "${d}" | sed "s/\/$//"`
+	remote="git@github.com:mfwolffe/${remote}.git"
+	
+	pushd "${d}" >/dev/null
+
+	printf "Sanitizing yielded remote: "
+	tput bold;
+	tput setaf 5;
+	printf "%s\n" $remote ;
+	tput sgr0;
+	
+	printf "Add this remote for current repository: " ;
+	tput bold;
+	tput setaf 6;
+	printf "%s\n" $d
+	tput sgr0
+	
+	select yn in "Yes" "No"; do
+	    case $yn in
+	        Yes ) remote_adder $remote; break;;
+	        No ) exit;;
+	    esac
+	done
+
+	popd >/dev/null
+
+done ;
