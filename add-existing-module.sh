6f6f3c9eedb609e120bd04b22166d5ba02782304
mfwolffe <wolffemf@dukes.jmu.edu>
2025-01-14 17:01:11 -0500

    tiny bit of documentation

--- add-existing-module.sh
+++ add-existing-module.sh
@@ -3,8 +3,8 @@
 set -x
 
 if [ ! $# -eq 2 ]; then 
-  printf "\nmissing new module name\n" >&2
-  exit 1
+  printf "\nmissing new module name\n" >&2 ;
+  exit 1 ;
 fi
 
 module_ref="${1}"
@@ -14,12 +14,14 @@ case "${1}" in
   "ssh" | "SSH")
     module_url="git@github.com:mfwolffe/${module_ref}.git" ;;
   "https" | "HTTPS")
-    module_url="https://github.com/mfwolffe/${module_ref}.git"
+    module_url="https://github.com/mfwolffe/${module_ref}.git" ;;
 esac
 
 git submodule add "${module_url}"
+
+# --init flag will clone the repo too
+# --recursive will do the same for any sub-submodules
 git submodule update --init --recursive
 
 git add "${module_ref}"
-
 
